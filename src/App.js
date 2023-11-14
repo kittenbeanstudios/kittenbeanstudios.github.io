3be052c6511c41ec7b5f45b1a48fda65203097ce
hanserin <serinstellahan@gmail.com>
2023-11-13 18:15:48 -0800

    React Readme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, { useState, useEffect } from "react";
+import ReactMarkdown from 'react-markdown'
+import readme from './README.md'
 
 function App() {
+  const [markdown, setMarkdown] = useState("");
+
+  useEffect(() => {
+    fetch(readme)
+      .then((res) => res.text())
+      .then((text) => setMarkdown(text));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactMarkdown className="Readme" children={markdown} />
     </div>
   );
 }
